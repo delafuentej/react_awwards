486e42bc75dbe2b81a8922445de8675fa5a09661
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-05-10 11:29:52 +0200

    first commit: setting-up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1 className="text-violet-300 font-bold text-5xl">
+        Welcome to Awwards WebSeite
+      </h1>
+    </main>
+  );
+}
+
+export default App;
